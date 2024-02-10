ede4dbe40e2dda39e255e84b5ee948720805463c
Doy Kim <kimdee.dev@gmail.com>
2024-02-10 20:25:16 +0900

    Create 19539_사과나무.swift

---
+++ 2회차/김도이/19539_사과나무.swift
@@ -0,0 +1,18 @@
+/*
+참고: https://readble-ko.tistory.com/149
+문제 이해가 잘 안되어서 참고.. ㅠ 
+*/
+
+let n = Int(readLine()!)! // 사과나무갯수
+var array = readLine()!.split(separator:" ").map { Int(String($0))!}
+var count = 0
+var sum = 0
+
+for i in 0..<n {
+    sum += array[i]
+    count += array[i]/2
+}
+
+if (sum % 3 != 0 ) { print("NO") }
+else if (count >= sum/3) { print("YES") } 
+else { print("NO")}
