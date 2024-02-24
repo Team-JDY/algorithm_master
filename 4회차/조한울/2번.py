20f6c6125df6e00dd89993ec86319cf1b660b52d
DeepSky <deepskyai1215@gmail.com>
2024-02-24 20:24:12 +0900

    4회차 2번

--- 4회차/조한울/2번.py
+++ 4회차/조한울/2번.py
@@ -112,6 +112,9 @@ def solution4(n, m, x, y, r, c, k):
 
 solution4(3,4,2,3,3,1,5)
 
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
 
 def solution5(n, m, x, y, r, c, k):
     manhattan = abs(x - r) + abs(y - c)
@@ -122,23 +125,25 @@ def solution5(n, m, x, y, r, c, k):
     direction = [(1, 0), (0, -1), (0, 1), (-1, 0)]
     index = ["d", "l", "r", "u"]
 
-    stack = [[x, y, 0, ""]]
-    while stack:
-        now_x, now_y, move_count, path = stack.pop()
-        if best_routes < path:
-            continue
+    def detect(dx, dy, cnt, path, limit):
+        nonlocal best_routes
+        if path > best_routes:
+            return
 
-        if move_count > k:
-            continue
+        rm, dt = limit - cnt, abs(dx - r) + abs(dy - c)
+        if dt > rm or (rm - dt) % 2 == 1:
+            return
 
-        if move_count == k and (now_x, now_y) == (r, c):
+        if cnt == limit and (dx, dy) == (r, c):
             best_routes = path
-            continue
+            return
 
-        for i, (dx, dy) in enumerate(direction):
-            next_x, next_y = now_x + dx, now_y + dy
+        for i, (j, k) in enumerate(direction):
+            next_x, next_y = dx + j, dy + k
             if 1 <= next_x <= n and 1 <= next_y <= m:
-                stack.append([next_x, next_y, move_count + 1, path + index[i]])
+                detect(next_x, next_y, cnt+1, path + index[i], limit)
+
+    detect(x, y, 0, "", k)
 
     return best_routes
 
